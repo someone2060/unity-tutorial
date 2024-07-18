656bc8799c03d9dd61b960366cd91cf14cd0b6d5
Daniel Liang <74940473+someone2060@users.noreply.github.com>
2024-07-17 22:41:41 -0600

    Create Player.cs
    just following orders

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using UnityEngine.Serialization;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 1.0f;
+
+    private void Update()
+    {
+        Vector2 inputVector = new Vector2(0.0f, 0.0f);
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector.y = 1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y = -1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x = -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x = 1;
+        }
+
+        inputVector = inputVector.normalized;
+
+        Vector3 moveDirection = new Vector3(inputVector.x, 0.0f, inputVector.y);
+        transform.position += moveDirection * (moveSpeed * Time.deltaTime);
+        Debug.Log(inputVector);
+    }
+}
