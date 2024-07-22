28e0bb794745e0681e5b52721aab436ad314a548
Daniel Liang <74940473+someone2060@users.noreply.github.com>
2024-07-22 11:32:34 -0600

    Refactor player movement to GameInput.cs
    Done for clean code

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -2,17 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class GameInput : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Vector2 GetMovementVectorNormalized()
     {
+        Vector2 inputVector = new Vector2(0.0f, 0.0f);
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector.y = 1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y = -1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x = -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x = 1;
+        }
         
-    }
+        inputVector = inputVector.normalized;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        return inputVector;
     }
 }
