7f1d91c16cde918d46de829064c01344823e6a5f
Daniel Liang <74940473+someone2060@users.noreply.github.com>
2024-07-22 20:30:54 -0600

    Implement new input system

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -4,28 +4,21 @@ using UnityEngine;
 
 public class GameInput : MonoBehaviour
 {
+    private PlayerInputActions _playerInputActions;
+
+    private void Awake()
+    {
+        _playerInputActions = new PlayerInputActions();
+        _playerInputActions.Player.Enable();
+    }
+    
     public Vector2 GetMovementVectorNormalized()
     {
-        Vector2 inputVector = new Vector2(0.0f, 0.0f);
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = 1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = 1;
-        }
+        Vector2 inputVector = _playerInputActions.Player.Move.ReadValue<Vector2>();
         
         inputVector = inputVector.normalized;
 
+        Debug.Log(inputVector);
         return inputVector;
     }
 }
