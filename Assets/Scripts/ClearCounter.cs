3b047f150e90081c230ee1b44a5c315995ccfaf1
Daniel Liang <74940473+someone2060@users.noreply.github.com>
2024-07-24 16:34:33 -0600

    Create SO for kitchen objects
    Also added code placing prefab on counter when interacted with

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class ClearCounter : MonoBehaviour
 {
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+    [SerializeField] private Transform counterTopPoint;
+    
     public void Interact()
     {
         Debug.Log("Interacting");
+        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+        kitchenObjectTransform.localPosition = Vector3.zero;
     }
 }
