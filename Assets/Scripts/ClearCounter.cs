45cae7284d0d624a368c523ff76e06a4b99b4c36
Daniel Liang <74940473+someone2060@users.noreply.github.com>
2024-07-26 16:45:31 -0600

    `ContainerCounter`: implement OpenClose animation

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -6,8 +6,6 @@ using UnityEngine.Serialization;
 
 public class ClearCounter : BaseCounter
 {
-    [SerializeField] private KitchenObjectSO kitchenObjectSO;
-    
     public override void Interact(Player player)
     {
         // code
--- Assets/Scripts/ContainerCounter.cs
+++ Assets/Scripts/ContainerCounter.cs
@@ -1,20 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ContainerCounter : BaseCounter
 {
+    public EventHandler OnPlayerGrabbedObject;
+    
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
     
     public override void Interact(Player player)
     {
-        if (!HasKitchenObject())
-        {
-            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
-            kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
-            return;
-        }
+        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
+        kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
         
-        GetKitchenObject().SetKitchenObjectParent(player);
+        OnPlayerGrabbedObject?.Invoke(this, EventArgs.Empty);
     }
 }
