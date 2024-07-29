7dcb9e2c97c60c052e95c021054facd8ced4a161
Daniel Liang <74940473+someone2060@users.noreply.github.com>
2024-07-29 17:30:26 -0600

    Fix `CuttingRecipeSO.cs` bug

--- Assets/Scripts/CuttingRecipeSO.cs
+++ Assets/Scripts/CuttingRecipeSO.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "CuttingRecipe", menuName = "Scriptable Objects/Cutting Recipe")]
-public class NewBehaviourScript : ScriptableObject
+public class CuttingRecipeSO : ScriptableObject
 {
     public KitchenObjectSO input, output;
 }
