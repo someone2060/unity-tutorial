4db358ab72bcd4e7b9758952e1f3c90a8aeaa7ec
Daniel Liang <74940473+someone2060@users.noreply.github.com>
2024-07-29 12:55:37 -0600

    Create `CuttingCounter.cs`: copy `ClearCounter.cs` code

---
+++ Assets/Scripts/CuttingCounter.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CuttingCounter : BaseCounter
+{
+    public override void Interact(Player player)
+    {
+        if (HasKitchenObject())
+        {
+            if (!player.HasKitchenObject())
+            {
+                GetKitchenObject().SetKitchenObjectParent(player);
+            }
+            return;
+        }
+
+        if (player.HasKitchenObject())
+        {
+            player.GetKitchenObject().SetKitchenObjectParent(this);
+        }
+    }
+}
