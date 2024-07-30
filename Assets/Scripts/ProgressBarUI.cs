62b612009b9e18a06be8f602b33d9d9c1d238562
Daniel Liang <74940473+someone2060@users.noreply.github.com>
2024-07-30 16:20:52 -0600

    `ProgressBarUI.cs`: Update visual on cut

--- Assets/Scripts/ProgressBarUI.cs
+++ Assets/Scripts/ProgressBarUI.cs
@@ -8,4 +8,15 @@ public class ProgressBarUI : MonoBehaviour
 {
     [SerializeField] private CuttingCounter cuttingCounter;
     [SerializeField] private Image barImage;
+
+    private void Start()
+    {
+        cuttingCounter.OnProgressChanged += CuttingCounter_OnProgressChanged;
+        barImage.fillAmount = 0.0f;
+    }
+
+    private void CuttingCounter_OnProgressChanged(object sender, CuttingCounter.OnProgressChangedEventArgs e)
+    {
+        barImage.fillAmount = e.ProgressNormalized;
+    }
 }
