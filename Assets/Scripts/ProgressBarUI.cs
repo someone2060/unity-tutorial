147e4de754dfc526c3cb7cc23608502b2a765125
Daniel Liang <74940473+someone2060@users.noreply.github.com>
2024-07-30 16:29:44 -0600

    `ProgressBarUI.cs`: Hide if bar is full/empty

--- Assets/Scripts/ProgressBarUI.cs
+++ Assets/Scripts/ProgressBarUI.cs
@@ -12,11 +12,32 @@ public class ProgressBarUI : MonoBehaviour
     private void Start()
     {
         cuttingCounter.OnProgressChanged += CuttingCounter_OnProgressChanged;
+        
         barImage.fillAmount = 0.0f;
+        
+        Hide();
     }
 
     private void CuttingCounter_OnProgressChanged(object sender, CuttingCounter.OnProgressChangedEventArgs e)
     {
         barImage.fillAmount = e.ProgressNormalized;
+
+        if (e.ProgressNormalized == 0f || Mathf.Approximately(e.ProgressNormalized, 1f))
+        {
+            Hide();
+        } else
+        {
+            Show();
+        }
+    }
+
+    private void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    private void Hide()
+    {
+        gameObject.SetActive(false);
     }
 }
