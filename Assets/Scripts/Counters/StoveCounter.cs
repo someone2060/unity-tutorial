7affaa46ae5a52b93db554be78a711dd0e95f387
Daniel Liang <74940473+someone2060@users.noreply.github.com>
2024-07-31 17:05:45 -0600

    `StoveCounter.cs`: Cook patty while on stove

--- Assets/Scripts/Counters/StoveCounter.cs
+++ Assets/Scripts/Counters/StoveCounter.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,7 +7,28 @@ using UnityEngine.Serialization;
 public class StoveCounter : BaseCounter
 {
     [SerializeField] private FryingRecipeSO[] fryingRecipeSOArray;
-    private float _fryingProgress;
+    
+    private float _fryingTimer;
+    private FryingRecipeSO _fryingRecipeSO;
+
+    private void Update()
+    {
+        // Counter doesn't have KitchenObject
+        if (!HasKitchenObject()) return;
+
+        _fryingTimer += Time.deltaTime;
+
+        _fryingRecipeSO = GetFryingRecipeSO(GetKitchenObject().GetKitchenObjectSO());
+        if (!HasRecipeWithInput(_fryingRecipeSO)) return;
+
+        // Not fried
+        if (_fryingTimer <= _fryingRecipeSO.fryingTimerMax) return;
+
+        _fryingTimer = 0.0f;
+        Debug.Log("Fried!");
+        GetKitchenObject().DestroySelf();
+        KitchenObject.SpawnKitchenObject(_fryingRecipeSO.output, this);
+    }
 
     public override void Interact(Player player)
     {
@@ -21,7 +43,7 @@ public class StoveCounter : BaseCounter
 
         if (!player.HasKitchenObject()) return; // Player doesn't have KitchenObject
 
-        FryingRecipeSO fryingRecipeSO = GetCuttingRecipeSO(player.GetKitchenObject().GetKitchenObjectSO());
+        FryingRecipeSO fryingRecipeSO = GetFryingRecipeSO(player.GetKitchenObject().GetKitchenObjectSO());
         // Player placing object that isn't CuttingRecipeSO.input
         if (!HasRecipeWithInput(fryingRecipeSO)) return;
 
@@ -32,7 +54,7 @@ public class StoveCounter : BaseCounter
 
     private bool HasRecipeWithInput(KitchenObjectSO inputKitchenObjectSO)
     {
-        FryingRecipeSO fryingRecipeSO = GetCuttingRecipeSO(inputKitchenObjectSO);
+        FryingRecipeSO fryingRecipeSO = GetFryingRecipeSO(inputKitchenObjectSO);
         return fryingRecipeSO is not null;
     }
 
@@ -43,13 +65,13 @@ public class StoveCounter : BaseCounter
 
     private KitchenObjectSO GetOutputKitchenObject(KitchenObjectSO inputKitchenObjectSO)
     {
-        FryingRecipeSO fryingRecipeSO = GetCuttingRecipeSO(inputKitchenObjectSO);
+        FryingRecipeSO fryingRecipeSO = GetFryingRecipeSO(inputKitchenObjectSO);
         
         if (fryingRecipeSO is not null) return fryingRecipeSO.output;
         return null;
     }
 
-    private FryingRecipeSO GetCuttingRecipeSO(KitchenObjectSO inputKitchenObjectSO)
+    private FryingRecipeSO GetFryingRecipeSO(KitchenObjectSO inputKitchenObjectSO)
     {
         foreach (FryingRecipeSO fryingRecipeSO in fryingRecipeSOArray)
         {
