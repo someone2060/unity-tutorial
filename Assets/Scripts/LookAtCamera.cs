dedad2c2e164feb401407bf70ad71c3c3647c389
Daniel Liang <74940473+someone2060@users.noreply.github.com>
2024-07-30 19:19:56 -0600

    Add enums to `LookAtCamera.cs`
    Generic script with multiple options

--- Assets/Scripts/LookAtCamera.cs
+++ Assets/Scripts/LookAtCamera.cs
@@ -6,8 +6,33 @@ using Debug = System.Diagnostics.Debug;
 
 public class LookAtCamera : MonoBehaviour
 {
+    private enum Mode
+    {
+        LookAt,
+        LookAtInverted,
+        CameraForward,
+        CameraForwardInverted
+    }
+
+    [SerializeField] private Mode mode;
+    
     private void LateUpdate()
     {
-        transform.LookAt(Camera.main.transform);
+        switch (mode)
+        {
+            case Mode.LookAt:
+                transform.LookAt(Camera.main.transform);
+                break;
+            case Mode.LookAtInverted:
+                Vector3 directionFromCamera = transform.position - Camera.main.transform.position;
+                transform.LookAt(transform.position + directionFromCamera);
+                break;
+            case Mode.CameraForward:
+                transform.forward = Camera.main.transform.forward;
+                break;
+            case Mode.CameraForwardInverted:
+                transform.forward = -Camera.main.transform.forward;
+                break;
+        }
     }
 }
