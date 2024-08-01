ab8ed12a3aa1108eee43627c06c944b8eb6f33f4
Daniel Liang <74940473+someone2060@users.noreply.github.com>
2024-07-31 19:25:37 -0600

    Create `PlatesCounter` prefab

---
+++ Assets/Scripts/Counters/PlatesCounter.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatesCounter : BaseCounter
+{
+    public override void Interact(Player player)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void InteractAlternate(Player player)
+    {
+        throw new System.NotImplementedException();
+    }
+}
