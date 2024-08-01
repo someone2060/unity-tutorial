106ebbc5354dbcc4d8d7ae3f12d6a04868ab36db
Daniel Liang <74940473+someone2060@users.noreply.github.com>
2024-07-31 18:45:23 -0600

    Create `IHasProgress.cs` interface

---
+++ Assets/Scripts/Interfaces/IHasProgress.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Objects that implement this interface will have some kind of progress behaviour.
+ */
+public interface IHasProgress
+{
+    public event EventHandler OnCut;
+    public event EventHandler<OnProgressChangedEventArgs> OnProgressChanged;
+    public class OnProgressChangedEventArgs : EventArgs
+    {
+        public float ProgressNormalized;
+    }
+}
--- Assets/Scripts/ProgressBarUI.cs
+++ Assets/Scripts/ProgressBarUI.cs
@@ -6,19 +6,27 @@ using UnityEngine.UI;
 
 public class ProgressBarUI : MonoBehaviour
 {
-    [SerializeField] private CuttingCounter cuttingCounter;
+    [SerializeField] private GameObject hasProgressGameObject;
     [SerializeField] private Image barImage;
 
+    IHasProgress _hasProgress;
+    
     private void Start()
     {
-        cuttingCounter.OnProgressChanged += CuttingCounter_OnProgressChanged;
+        _hasProgress = hasProgressGameObject.GetComponent<IHasProgress>();
+        if (_hasProgress is null)
+        {
+            Debug.LogError($"Game Object {hasProgressGameObject} does not have a component that implements IHasProgress!");
+        }
+        
+        _hasProgress.OnProgressChanged += HasProgress_OnProgressChanged;
         
         barImage.fillAmount = 0.0f;
         
         Hide();
     }
 
-    private void CuttingCounter_OnProgressChanged(object sender, CuttingCounter.OnProgressChangedEventArgs e)
+    private void HasProgress_OnProgressChanged(object sender, IHasProgress.OnProgressChangedEventArgs e)
     {
         barImage.fillAmount = e.ProgressNormalized;
 
