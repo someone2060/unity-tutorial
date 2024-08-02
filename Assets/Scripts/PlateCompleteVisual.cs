ca82041918d5638139348bc6e4e2924dbae09fc5
Daniel Liang <74940473+someone2060@users.noreply.github.com>
2024-08-02 16:08:37 -0600

    "Create" `PlateCompleteVisual` prefab

---
+++ Assets/Scripts/PlateCompleteVisual.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class PlateCompleteVisual : MonoBehaviour
+{
+    [SerializeField] private PlateKitchenObject plateKitchenObject;
+}
