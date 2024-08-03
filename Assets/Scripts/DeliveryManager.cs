d1916025735e12c22d8bbe03b63124f28051e650
Daniel Liang <74940473+someone2060@users.noreply.github.com>
2024-08-02 19:25:13 -0600

    `DeliveryManager`: Add recipes to be fulfilled

--- Assets/Scripts/DeliveryManager.cs
+++ Assets/Scripts/DeliveryManager.cs
@@ -1,10 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class DeliveryManager : MonoBehaviour
 {
     [SerializeField] private RecipeListSO recipeListSO;
     
     private List<RecipeSO> _waitingRecipeSOList;
+    private float _spawnRecipeTimer;
+    private const float SpawnRecipeTimerMax = 4.0f;
+    private const int WaitingRecipesMax = 4;
+
+    private void Awake()
+    {
+        _waitingRecipeSOList = new List<RecipeSO>();
+    }
+
+    private void Update()
+    {
+        if (_waitingRecipeSOList.Count >= WaitingRecipesMax) return; // Reached waiting max
+        
+        _spawnRecipeTimer -= Time.deltaTime;
+        if (_spawnRecipeTimer > 0) return; // Not enough time elapsed
+
+        _spawnRecipeTimer = SpawnRecipeTimerMax;
+
+        RecipeSO waitingRecipeSO = recipeListSO.recipeSOList[Random.Range(0, recipeListSO.recipeSOList.Count-1)];
+        Debug.Log(waitingRecipeSO.recipeName); //DEBUG
+        _waitingRecipeSOList.Add(waitingRecipeSO);
+    }
 }
