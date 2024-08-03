d7db6a106067d239075765e2f4fb86747eb68379
Daniel Liang <74940473+someone2060@users.noreply.github.com>
2024-08-02 19:04:39 -0600

    Create `RecipeSO` scriptable object

--- Assets/Scripts/DeliveryManager.cs
+++ Assets/Scripts/DeliveryManager.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class DeliveryManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private List<RecipeSO> _waitingRecipeSOList;
 }
