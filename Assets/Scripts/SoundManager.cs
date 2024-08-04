2d248d7247eb20d1aaf08f5dd72746d290fb40a7
Daniel Liang <74940473+someone2060@users.noreply.github.com>
2024-08-03 21:30:56 -0600

    Add SFX for delivery success/fail

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -1,9 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class SoundManager : MonoBehaviour
 {
+    [SerializeField] private AudioClipReferencesSO audioClipReferencesSO;
+    
+    private void Start()
+    {
+        DeliveryManager.Instance.OnRecipeSuccess += DeliveryManager_OnRecipeSuccess;
+        DeliveryManager.Instance.OnRecipeFailed += DeliveryManager_OnRecipeFailed;
+    }
+
+    private void DeliveryManager_OnRecipeSuccess(object sender, EventArgs e)
+    {
+        PlaySound(audioClipReferencesSO.deliverySuccess, Camera.main.transform.position);
+    }
+
+    private void DeliveryManager_OnRecipeFailed(object sender, EventArgs e)
+    {
+        PlaySound(audioClipReferencesSO.deliveryFail, Camera.main.transform.position);
+    }
+
+    private void PlaySound(AudioClip[] audioClipArray, Vector3 position, float volume = 1.0f)
+    {
+        PlaySound(audioClipArray[Random.Range(0, audioClipArray.Length)], position, volume);
+    }
+
     private void PlaySound(AudioClip audioClip, Vector3 position, float volume = 1.0f)
     {
         AudioSource.PlayClipAtPoint(audioClip, position, volume);
