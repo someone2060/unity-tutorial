8bb450a02da9c93c636e7ce2c0badb95ddc62734
Daniel Liang <74940473+someone2060@users.noreply.github.com>
2024-08-07 19:30:24 -0600

    `GamePauseUI`: Implement menu traversal with `OptionsUI`

--- Assets/Scripts/UI/OptionsUI.cs
+++ Assets/Scripts/UI/OptionsUI.cs
@@ -7,13 +7,18 @@ using UnityEngine.UI;
 
 public class OptionsUI : MonoBehaviour
 {
+    public static OptionsUI Instance { get; private set; }
+    
     [SerializeField] private Button musicButton;
     [SerializeField] private Button soundEffectsButton;
+    [SerializeField] private Button backButton;
     [SerializeField] private TextMeshProUGUI musicText;
     [SerializeField] private TextMeshProUGUI soundEffectsText;
 
     private void Awake()
     {
+        Instance = this;
+        
         musicButton.onClick.AddListener((() =>
         {
             MusicManager.Instance.ChangeVolume();
@@ -24,16 +29,39 @@ public class OptionsUI : MonoBehaviour
             SoundManager.Instance.ChangeVolume();
             UpdateVisual();
         }));
+        backButton.onClick.AddListener((() =>
+        {
+            GamePauseUI.Instance.Show();
+            Hide();
+        }));
     }
 
     private void Start()
     {
+        GameManager.Instance.OnGameUnpaused += GameManager_OnGameUnpaused;
         UpdateVisual();
+        
+        Hide();
+    }
+
+    private void GameManager_OnGameUnpaused(object sender, EventArgs e)
+    {
+        Hide();
     }
 
     private void UpdateVisual()
     {
         musicText.text = "Music: " + Mathf.Round(MusicManager.Instance.GetVolume() * 10.0f);
         soundEffectsText.text = "Sound Effects: " + Mathf.Round(SoundManager.Instance.GetVolume() * 10.0f);
+    }
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
     }
 }
